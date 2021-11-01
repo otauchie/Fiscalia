cf28f9347ced913b0803ecc5097c301d3408240b
otauchie <otauchie@gmail.com>
2021-11-01 20:32:58 -0300

    creadas el resto de las clases y sus relaciones

--- src/main/java/ar/edu/unnoba/poo2021/Fiscalia/model/Causa.java
+++ src/main/java/ar/edu/unnoba/poo2021/Fiscalia/model/Causa.java
@@ -1,4 +1,79 @@
 package ar.edu.unnoba.poo2021.Fiscalia.model;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Causas")
 public class Causa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "numero_expediente")
+    private String numeroExpediente;
+
+    @Column(name = "juzgado")
+    private String juzgado;
+
+    @ManyToOne
+    @JoinColumn(name = "persona_id")
+    private Persona victima;
+
+    @ManyToMany
+    @JoinTable(name = "informacion_causas",
+    joinColumns=@JoinColumn(name = "causa_id"),
+    inverseJoinColumns = @JoinColumn(name = "informacion_id"))
+    private List<Informacion> informaciones;
+
+    @Column(name = "contexto")
+    private String contexto;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumeroExpediente() {
+        return numeroExpediente;
+    }
+
+    public void setNumeroExpediente(String numeroExpediente) {
+        this.numeroExpediente = numeroExpediente;
+    }
+
+    public String getJuzgado() {
+        return juzgado;
+    }
+
+    public void setJuzgado(String juzgado) {
+        this.juzgado = juzgado;
+    }
+
+    public Persona getVictima() {
+        return victima;
+    }
+
+    public void setVictima(Persona victima) {
+        this.victima = victima;
+    }
+
+    public List<Informacion> getInformaciones() {
+        return informaciones;
+    }
+
+    public void setInformaciones(List<Informacion> informaciones) {
+        this.informaciones = informaciones;
+    }
+
+    public String getContexto() {
+        return contexto;
+    }
+
+    public void setContexto(String contexto) {
+        this.contexto = contexto;
+    }
 }
