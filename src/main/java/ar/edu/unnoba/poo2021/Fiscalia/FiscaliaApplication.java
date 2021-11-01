af48f69817141c2a96c8539ff1419ee45abfe862
otauchie <otauchie@gmail.com>
2021-11-01 17:39:44 -0300

    inicio de proyecto

---
+++ src/main/java/ar/edu/unnoba/poo2021/Fiscalia/FiscaliaApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo2021.Fiscalia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiscaliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiscaliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unnoba/poo2021/Fiscalia/FiscaliaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo2021.Fiscalia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiscaliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
