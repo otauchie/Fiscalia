69ca4c097437040da74053f59de9a186d9e63601
yesiruzzi <yesicaruzzi@hotmail.com>
2021-11-04 15:56:24 -0300

    se borro la clase persona y sus relacionados

--- src/main/java/ar/edu/unnoba/poo2021/Fiscalia/repository/PersonaRepository.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.unnoba.poo2021.Fiscalia.repository;
-
-import ar.edu.unnoba.poo2021.Fiscalia.model.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonaRepository extends JpaRepository<Persona,Long> {
-    public Persona findByNombre(String nombre);
-}
