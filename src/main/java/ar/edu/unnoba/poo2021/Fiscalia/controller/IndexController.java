81feb4ef55ed70999fa388bab041b3ac999335c2
otauchie <otauchie@gmail.com>
2022-01-28 20:31:04 -0300

    cambios esteticos

--- src/main/java/ar/edu/unnoba/poo2021/Fiscalia/controller/IndexController.java
+++ src/main/java/ar/edu/unnoba/poo2021/Fiscalia/controller/IndexController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping("/")
     public String Index(){
-        return "index";
+        return "redirect:causas/listaCausas";
     }
 }
